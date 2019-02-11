0fe3cf3b0028f27ba2d2379d4214e5a6152f9be8
Maciej Kućmierowski <35165148+maciekKuc@users.noreply.github.com>
2019-02-11 22:37:06 +0100

    minor functionality upgrade
    Added a functionality to change the background dependent from the weather, and some minor styling

--- weatherAPP.js
+++ weatherAPP.js
@@ -10,13 +10,13 @@ let crd = 'a';
 //end od starter declarations//
 
   
-var options = {
+let options = {
   enableHighAccuracy: true,
   timeout: 5000,
   maximumAge: 0
 };
 
-var weatherAPI = 'https://api.openweathermap.org/data/2.5/weather?lat=';
+let weatherAPI = 'https://api.openweathermap.org/data/2.5/weather?lat=';
 
 function success(pos) {
   crd = pos.coords;
@@ -38,15 +38,29 @@ navigator.geolocation.getCurrentPosition(success, error, options);
   
   //the weather API//
   //var weatherAPI = 'https://api.openweathermap.org/data/2.5/weather?q=London&APPID=6f502ba12afc62626fa3e95fc8413d94';
-const weatherAPI = 'https://api.openweathermap.org/data/2.5/weather?lat=';
  
-var data = {
+let data = {
   //q : "Portland,OR",
   units : "metric"
 };
 
 function showWeather (weatherReport) {
+  console.log(weatherReport);
    temp = weatherReport.main.temp;
+ 
+   console.log('ok');
+}
+
+const getWeatherImg = function(){
+    if(temp >= 30){
+      document.getElementById('main').style.backgroundImage = "url('./img/sunny.jpg')";
+   }else if(temp >= 15 && temp < 30){
+      document.getElementById('main').style.backgroundImage = "url('./img/spring.jpg')";
+   }else if(temp >= 0 && temp < 15){
+      document.getElementById('main').style.backgroundImage = "url('./img/rainy.jpg')";
+   }else if(temp < 0){
+      document.getElementById('main').style.backgroundImage = "url('./img/snowy.jpg')";
+   }
 }
   //$.getJSON(weatherAPI, data, showWeather);
   //the end of the weather API//
@@ -55,17 +69,18 @@ function showWeather (weatherReport) {
 celsius.addEventListener('click', (event) => {
   let tempCelsius = temp.toFixed(2);
   if (event.target.tagName == 'BUTTON'){
-    let showCelsius = '<h6 class="text-center text-light">Temperature near you: ' + tempCelsius + ' &#8451</h6>';
+    let showCelsius = '<h6 class="temperature">Temperature near you: ' + tempCelsius + ' &#8451</h6>';
     weather.innerHTML = showCelsius ;
   }
+  getWeatherImg();
 });
 
 fahrenheit.addEventListener('click', (event) => {
   let tempFahrenheit = ((9/5) * temp + 32).toFixed(2);
   if (event.target.tagName == 'BUTTON'){
-    let showFahrenheit = '<h6 class="text-center text-light">Temperature near you: ' + tempFahrenheit + ' &#8457</h6>';
+    let showFahrenheit = '<h6 class="temperature">Temperature near you: ' + tempFahrenheit + ' &#8457</h6>';
     weather.innerHTML = showFahrenheit ;
   }
   });
-  console.log(lon);
+  getWeatherImg();
 });
